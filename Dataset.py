1a7a7132737bd9d8534b388b3c8cbc5b15be67a0
Lorenzo Perini <lorenzoperini@meta.com>
2025-07-17 09:23:58 +0100

    Upload first version of CAS MCBoost

--- Dataset.py
+++ Dataset.py
@@ -1,33 +1,37 @@
-from dataloaders.Adult import load_AdultIncome
+import numpy as np
+import pandas as pd
+
+# import torchtext
 from dataloaders.ACS import load_ACSIncome, load_ACSIncome_no_race
+from dataloaders.Adult import load_AdultIncome
+from dataloaders.AmazonPolarity import load_AmazonPolarity
+from dataloaders.BankMarketing import load_BankMarketing, load_BankMarketing_no_job
+from dataloaders.Camelyon17 import load_Camelyon17
+from dataloaders.CelebA import load_CelebA
+from dataloaders.CivilComments import load_CivilComments
 from dataloaders.CreditDefault import load_CreditDefault, load_CreditDefault_no_edu
 from dataloaders.HMDA import load_processed_HMDA, load_processed_HMDA_no_race
-from dataloaders.BankMarketing import load_BankMarketing, load_BankMarketing_no_job
 from dataloaders.MEPS import load_MEPS, load_MEPS_no_pov
-from dataloaders.CivilComments import load_CivilComments
-from dataloaders.YelpPolarity import load_YelpPolarity
-from dataloaders.AmazonPolarity import load_AmazonPolarity
-from dataloaders.CelebA import load_CelebA
 from dataloaders.WaterBirds import load_WaterBirds
-from dataloaders.Camelyon17 import load_Camelyon17
-from sklearn.preprocessing import StandardScaler
+from dataloaders.YelpPolarity import load_YelpPolarity
 from imblearn.over_sampling import SMOTE as imbl_smote
 from prettytable import PrettyTable
-import pandas as pd
-import numpy as np
-import torchtext
+from sklearn.preprocessing import StandardScaler
 
 
 class Dataset:
-    def __init__(self, dataset_name, 
-                 split={'train': 0.6, 'val': 0.2, 'test': 0.2},
-                 groups='default',
-                 scale=False,
-                 class_balance=False,
-                 val_split_seed=43, 
-                 min_group_size=0.005,
-                 verbose=True):
-        '''
+    def __init__(
+        self,
+        dataset_name,
+        split={"train": 0.6, "val": 0.2, "test": 0.2},
+        groups="default",
+        scale=False,
+        class_balance=False,
+        val_split_seed=43,
+        min_group_size=0.005,
+        verbose=True,
+    ):
+        """
         Parameters:
             :dataset_name: str, name of the dataset to load
             :split: dict, split fractions for train, val, test
@@ -35,10 +39,10 @@ class Dataset:
             :scale: bool, whether to scale the data to mean 0, std 1
             :class_balance: bool, whether to balance the classes in the training data using SMOTE
             :val_split_seed: int, seed for splitting the training data into train and validation
-            :min_group_size: float, minimum fraction of entire dataset that a group must have to 
+            :min_group_size: float, minimum fraction of entire dataset that a group must have to
                             be included in the subgroup metrics
             :verbose: bool, whether to print group information
-        '''
+        """
         self.name = dataset_name
         self.min_group_size = min_group_size
         self.val_split_seed = val_split_seed
@@ -49,73 +53,74 @@ class Dataset:
         self.scale = scale
         self.class_balance = class_balance
         load_data_fn = None
-        
+
         # standard datasets
-        if self.name == 'ACSIncome':
+        if self.name == "ACSIncome":
             load_data_fn = load_ACSIncome
-        elif self.name == 'CreditDefault':
+        elif self.name == "CreditDefault":
             load_data_fn = load_CreditDefault
-        elif self.name == 'HMDA':
+        elif self.name == "HMDA":
             load_data_fn = load_processed_HMDA
-        elif self.name == 'BankMarketing':
+        elif self.name == "BankMarketing":
             load_data_fn = load_BankMarketing
-        elif self.name == 'MEPS':
+        elif self.name == "MEPS":
             load_data_fn = load_MEPS
-        elif self.name == 'AdultIncome':
+        elif self.name == "AdultIncome":
             load_data_fn = load_AdultIncome
         # expressiveness experiments
-        elif self.name == 'ACSIncome_no_race':
+        elif self.name == "ACSIncome_no_race":
             load_data_fn = load_ACSIncome_no_race
-        elif self.name == 'CreditDefault_no_edu':
+        elif self.name == "CreditDefault_no_edu":
             load_data_fn = load_CreditDefault_no_edu
-        elif self.name == 'BankMarketing_no_job':
+        elif self.name == "BankMarketing_no_job":
             load_data_fn = load_BankMarketing_no_job
-        elif self.name == 'MEPS_no_pov':
+        elif self.name == "MEPS_no_pov":
             load_data_fn = load_MEPS_no_pov
-        elif self.name == 'HMDA_no_race':
+        elif self.name == "HMDA_no_race":
             load_data_fn = load_processed_HMDA_no_race
         # language datasets
-        elif self.name == 'CivilComments':
+        elif self.name == "CivilComments":
             self.is_text = True
             load_data_fn = load_CivilComments
-        elif self.name == 'YelpPolarity':
+        elif self.name == "YelpPolarity":
             self.is_text = True
             load_data_fn = load_YelpPolarity
-        elif self.name == 'AmazonPolarity':
+        elif self.name == "AmazonPolarity":
             self.is_text = True
             load_data_fn = load_AmazonPolarity
         # image datasets
-        elif self.name == 'CelebA':
+        elif self.name == "CelebA":
             load_data_fn = load_CelebA
-        elif self.name == 'WaterBirds':
+        elif self.name == "WaterBirds":
             load_data_fn = load_WaterBirds
-        elif self.name == 'Camelyon17':
+        elif self.name == "Camelyon17":
             load_data_fn = load_Camelyon17
         else:
-            raise ValueError('Unknown dataset name')
-        
+            raise ValueError("Unknown dataset name")
+
         self.X, self.y, (self.groups, self.group_names) = load_data_fn(groups=groups)
-        
+
         # scale or class balance data
         self._preprocess_data()
         self._check_data_leakage()
-        
-        # print groups and their sizes
-        if verbose: print(groups_info_str(self.X, self.y, self.groups, self.group_names))
 
+        # print groups and their sizes
+        if verbose:
+            print(groups_info_str(self.X, self.y, self.groups, self.group_names))
 
     def vocab(self, tokenizer, max_len, min_freq):
         """
         Returns vocabular for text datasets.
         """
-        assert self.is_text == True, 'Vocab method only associated with language datasets.'
+        assert (
+            self.is_text == True
+        ), "Vocab method only associated with language datasets."
         tokenized = pd.Series(self.X).apply(lambda x: tokenizer(x)[:max_len])
-        v = torchtext.vocab.build_vocab_from_iterator(tokenized, 
-                                                      min_freq=min_freq, 
-                                                      specials=['<pad>', '<unk>'])
-        v.set_default_index(v['<unk>'])
+        v = torchtext.vocab.build_vocab_from_iterator(
+            tokenized, min_freq=min_freq, specials=["<pad>", "<unk>"]
+        )
+        v.set_default_index(v["<unk>"])
         return v
-        
 
     def train_calibration_split(self, frac, train_overlap=0, seed=43):
         """
@@ -125,13 +130,14 @@ class Dataset:
             :seed: int, seed for splitting the training data into calibration and training data
         """
         (
-            self.calibration_idxs, 
-            self.groups_calib, 
-            self.train_idxs_no_calib, 
-            self.groups_train_no_calib
-        ) = split_data(self.train_idxs, self.groups, 
-                       int(len(self.train_idxs) * frac), seed=seed)
-        
+            self.calibration_idxs,
+            self.groups_calib,
+            self.train_idxs_no_calib,
+            self.groups_train_no_calib,
+        ) = split_data(
+            self.train_idxs, self.groups, int(len(self.train_idxs) * frac), seed=seed
+        )
+
         # update calibration and training data
         self.X_calib = self.X[self.calibration_idxs]
         self.y_calib = self.y[self.calibration_idxs]
@@ -140,16 +146,22 @@ class Dataset:
 
         # if desired, allow overlap between the training and calibration data
         if train_overlap > 0:
-            print(f"Introducing overlap between train / calibration data: {train_overlap:.2f}")
+            print(
+                f"Introducing overlap between train / calibration data: {train_overlap:.2f}"
+            )
 
             # draw some {train_overlap}-fraction of training data
             np.random.seed(seed)
             num_samples = int(len(self.train_idxs_no_calib) * train_overlap)
-            tc_idxs = np.random.choice(self.train_idxs_no_calib, num_samples, replace=False)
+            tc_idxs = np.random.choice(
+                self.train_idxs_no_calib, num_samples, replace=False
+            )
 
             # update calib set
             # note that reindex_group assumes sorted indices, so sort here
-            self.calibration_idxs = sorted(np.concatenate([self.calibration_idxs, tc_idxs]))
+            self.calibration_idxs = sorted(
+                np.concatenate([self.calibration_idxs, tc_idxs])
+            )
             self.X_calib = self.X[self.calibration_idxs]
             self.y_calib = self.y[self.calibration_idxs]
 
@@ -159,8 +171,14 @@ class Dataset:
                 g_idxs = reindex_group(self.calibration_idxs, group)
                 self.groups_calib.append(g_idxs)
 
-        return self.X_train_no_calib, self.y_train_no_calib, self.groups_train_no_calib, self.X_calib, self.y_calib, self.groups_calib
-
+        return (
+            self.X_train_no_calib,
+            self.y_train_no_calib,
+            self.groups_train_no_calib,
+            self.X_calib,
+            self.y_calib,
+            self.groups_calib,
+        )
 
     def _preprocess_data(self):
         """
@@ -187,38 +205,43 @@ class Dataset:
         # (We only support 2 classes for now)
         self.num_classes = len(np.unique(self.y))
         if self.num_classes != 2:
-            raise ValueError('Only binary classification supported')
-        
+            raise ValueError("Only binary classification supported")
+
         # construct train, val, test splits
         # always deterministically construct the test split.
         self.test_split_seed = 42
 
         # check for valid split
-        if self.train_test_val_split['test'] + self.train_test_val_split['val'] + self.train_test_val_split['train'] > 1:
-            raise ValueError('Split fractions must be at most 1')
-        
-        self.test_size = self.train_test_val_split['test']
-        self.val_size = self.train_test_val_split['val']
-        self.train_size = self.train_test_val_split['train']
+        if (
+            self.train_test_val_split["test"]
+            + self.train_test_val_split["val"]
+            + self.train_test_val_split["train"]
+            > 1
+        ):
+            raise ValueError("Split fractions must be at most 1")
+
+        self.test_size = self.train_test_val_split["test"]
+        self.val_size = self.train_test_val_split["val"]
+        self.train_size = self.train_test_val_split["train"]
 
         self.n = len(self.X)
-        (
-            self.test_idxs, 
-            self.groups_test, 
-            train_val_idxs, 
-            _
-        ) = split_data(np.arange(self.n), self.groups, 
-                       int(self.n * self.test_size), self.test_split_seed)
+        (self.test_idxs, self.groups_test, train_val_idxs, _) = split_data(
+            np.arange(self.n),
+            self.groups,
+            int(self.n * self.test_size),
+            self.test_split_seed,
+        )
         self.X_test = self.X[self.test_idxs]
         self.y_test = self.y[self.test_idxs]
 
-        (
-            self.val_idxs, 
-            self.groups_val, 
-            self.train_idxs, 
-            self.groups_train
-        ) = split_data(train_val_idxs, self.groups, 
-                       int(self.n * self.val_size), self.val_split_seed)
+        (self.val_idxs, self.groups_val, self.train_idxs, self.groups_train) = (
+            split_data(
+                train_val_idxs,
+                self.groups,
+                int(self.n * self.val_size),
+                self.val_split_seed,
+            )
+        )
         self.X_val = self.X[self.val_idxs]
         self.y_val = self.y[self.val_idxs]
 
@@ -243,7 +266,7 @@ class Dataset:
         X_test_scaled = scaler.transform(self.X_test)
 
         return X_train_scaled, X_val_scaled, X_test_scaled
-    
+
     def _smote(self):
         """Balance the classes using SMOTE."""
 
@@ -254,19 +277,23 @@ class Dataset:
 
         # invoke SMOTE to balance the classes
         sm = imbl_smote()
-        balanced_idxs, _ = sm.fit_resample(np.arange(len(self.y_train)).reshape(-1, 1), self.y_train)
+        balanced_idxs, _ = sm.fit_resample(
+            np.arange(len(self.y_train)).reshape(-1, 1), self.y_train
+        )
         X_train_balanced = self.X_train[balanced_idxs.flatten()]
         y_train_balanced = self.y_train[balanced_idxs.flatten()]
-        groups_train_balanced = [reindex_group(balanced_idxs.flatten(), group) for group in self.groups_train]
+        groups_train_balanced = [
+            reindex_group(balanced_idxs.flatten(), group) for group in self.groups_train
+        ]
 
         # record changes
-        space = ' ' * 8
+        space = " " * 8
         smt_lbl_cnt_0 = np.sum(self.y_train == 0)
         smt_lbl_cnt_1 = np.sum(self.y_train == 1)
         smt_lbl_tot = len(self.y_train)
-        
+
         # print
-        print('*' * 20)
+        print("*" * 20)
         print(f"SMOTE called to balance classes in training data.")
         # Calculate proportions for original label distribution
         orig_lbl_prop_0 = lbl_cnt_0 / lbl_tot
@@ -277,12 +304,23 @@ class Dataset:
         smote_prop_0_str = f"{smt_lbl_cnt_0/smt_lbl_tot:.2%}"
         smote_prop_1_str = f"{smt_lbl_cnt_1/smt_lbl_tot:.2%}"
         # Calculate padding for alignment
-        max_len = max(len(orig_prop_0_str), len(orig_prop_1_str), len(smote_prop_0_str), len(smote_prop_1_str))
+        max_len = max(
+            len(orig_prop_0_str),
+            len(orig_prop_1_str),
+            len(smote_prop_0_str),
+            len(smote_prop_1_str),
+        )
         # Print with aligned arrows
-        print(f"{space}{'Class 0:':<20} (orig) {orig_prop_0_str:>{max_len}} -> (SMOTE) {smote_prop_0_str:>{max_len}}")
-        print(f"{space}{'Class 1:':<20} (orig) {orig_prop_1_str:>{max_len}} -> (SMOTE) {smote_prop_1_str:>{max_len}}")
-        print(f"{space}{'# samples:':<20} (orig) {lbl_tot:>{max_len}} -> (SMOTE) {smt_lbl_tot}")
-        print('*' * 20)
+        print(
+            f"{space}{'Class 0:':<20} (orig) {orig_prop_0_str:>{max_len}} -> (SMOTE) {smote_prop_0_str:>{max_len}}"
+        )
+        print(
+            f"{space}{'Class 1:':<20} (orig) {orig_prop_1_str:>{max_len}} -> (SMOTE) {smote_prop_1_str:>{max_len}}"
+        )
+        print(
+            f"{space}{'# samples:':<20} (orig) {lbl_tot:>{max_len}} -> (SMOTE) {smt_lbl_tot}"
+        )
+        print("*" * 20)
 
         return X_train_balanced, y_train_balanced, groups_train_balanced
 
@@ -291,19 +329,18 @@ class Dataset:
         # check for overlap between train and test indices
         overlap = np.intersect1d(self.train_idxs, self.test_idxs)
         if len(overlap) > 0:
-            raise ValueError('Data leakage: Overlap between train and test')
+            raise ValueError("Data leakage: Overlap between train and test")
         # train and val
         overlap = np.intersect1d(self.train_idxs, self.val_idxs)
         if len(overlap) > 0:
-            raise ValueError('Data leakage: Overlap between train and val')
+            raise ValueError("Data leakage: Overlap between train and val")
         # val and test
         overlap = np.intersect1d(self.val_idxs, self.test_idxs)
         if len(overlap) > 0:
-            raise ValueError('Data leakage: Overlap between val and test')
-        
+            raise ValueError("Data leakage: Overlap between val and test")
+
     def groups_info_df(self):
         return groups_info_df(self.X, self.y, self.groups, self.group_names)
-    
 
 
 def split_data(idxs, groups, num_in_p1, seed):
@@ -323,7 +360,7 @@ def split_data(idxs, groups, num_in_p1, seed):
     for group in groups:
         p1_groups.append(reindex_group(p1_idxs, group))
         p2_groups.append(reindex_group(p2_idxs, group))
-    
+
     return p1_idxs, p1_groups, p2_idxs, p2_groups
 
 
@@ -331,14 +368,14 @@ def reindex_group(idxs, group_idxs):
     """
     Reindex the group indices to match the new split data.
     Take 0-indexed location of group_idxs in idxs.
-        For example, if idxs = [1, 2, 3, 4] and 
+        For example, if idxs = [1, 2, 3, 4] and
         group_idxs = [2, 4], we return [1, 3].
     Note that this function assumes the passed in idxs are
     already sorted, as otherwise the returned group indices will
     not be correct for the data chosen with unsorted indices.
     """
     # verify assumptions
-    assert (idxs == np.sort(idxs)).all(), 'idxs must be sorted'
+    assert (idxs == np.sort(idxs)).all(), "idxs must be sorted"
 
     new_group_idxs = []
     sorted_idxs = idxs
@@ -353,14 +390,14 @@ def reindex_group(idxs, group_idxs):
             i += 1
         else:
             j += 1
-    
+
     return new_group_idxs
 
 
 def groups_info_df(X, y, groups, group_names):
-    '''
+    """
     Get group information as a pandas dataframe.
-    '''
+    """
     data = []
 
     N_samples = len(X)
@@ -368,12 +405,16 @@ def groups_info_df(X, y, groups, group_names):
     for i, group in enumerate(groups):
         size = len(group)
         frac = size / len(X)
-        if size == 0: y_mean = 0.5
-        else: y_mean = np.mean(y[group])
-        data.append([group_names[i], size, f'{frac:.4f}', f'{y_mean:.4f}'])
-    
-    data.append(['Dataset', N_samples, f'{N_samples / len(X):.4f}', f'{np.mean(y):.4f}'])
-    df = pd.DataFrame(data, columns=['group name', 'n samples', 'fraction', 'y mean'])
+        if size == 0:
+            y_mean = 0.5
+        else:
+            y_mean = np.mean(y[group])
+        data.append([group_names[i], size, f"{frac:.4f}", f"{y_mean:.4f}"])
+
+    data.append(
+        ["Dataset", N_samples, f"{N_samples / len(X):.4f}", f"{np.mean(y):.4f}"]
+    )
+    df = pd.DataFrame(data, columns=["group name", "n samples", "fraction", "y mean"])
     return df
 
 
@@ -382,14 +423,13 @@ def groups_info_str(X, y, gps, gp_names):
     Print groups and their sizes.
     """
     ptab = PrettyTable()
-    ptab.field_names = ['idx', 'group name', 'n samples', 'fraction', 'y-mean']
+    ptab.field_names = ["idx", "group name", "n samples", "fraction", "y-mean"]
     df = groups_info_df(X, y, gps, gp_names)
     n = len(df)
 
     for i, row in df.iterrows():
-        ptab.add_row([i] + row.tolist(), divider=(i == n-2))
-    
-    ptab.align = 'r'
-    ptab.align['group name'] = 'l'
-    return ptab.get_string()
+        ptab.add_row([i] + row.tolist(), divider=(i == n - 2))
 
+    ptab.align = "r"
+    ptab.align["group name"] = "l"
+    return ptab.get_string()
--- configs/constants.py
+++ configs/constants.py
@@ -1,29 +1,26 @@
 import numpy as np
 
-'''
+"""
 Here, we store a number of constant values used throughout experiments.
 Primarily, we document default values for the hyperparameters of the MCB algorithms.
-'''
+"""
 
 # default splits and seeds
-SPLIT_DEFAULT = {'train': 0.6, 'val': 0.2, 'test': 0.2}
+SPLIT_DEFAULT = {"train": 0.6, "val": 0.2, "test": 0.2}
 SEEDS_DEFAULT = [15, 25, 35, 45, 55]
 SEEDS_REDUCED = [15, 25, 35]
 CALIB_FRACS_DEFAULT = [0, 0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0]
 CALIB_FRACS_REDUCED = [0, 0.2, 0.4]
-LARGER_MLPS = [
-    [128, 256, 128],
-    [128, 'BN', 256, 'BN', 128]
-]
+LARGER_MLPS = [[128, 256, 128], [128, "BN", 256, "BN", 128]]
 
 # default hyperparameters for HKRR
 HKRR_DEFAULT = [
     {
-        'type': 'HKRR',
-        'params': [
+        "type": "HKRR",
+        "params": [
             {
-                'lambda': 0.1,
-                'alpha': 0.1,
+                "lambda": 0.1,
+                "alpha": 0.1,
             },
             # {
             #     'lambda': 0.1,
@@ -37,8 +34,8 @@ HKRR_DEFAULT = [
             #     'lambda': 0.1,
             #     'alpha': 0.0125,
             # }
-        ]
-    }, 
+        ],
+    },
 ]
 
 # we use a reduced version of the sweep in Haghtalab et al. 2021
@@ -48,32 +45,40 @@ HJZ_other_lrs = [0.9, 0.95, 0.98, 0.99]
 HJZ_lr_pairs = [(a, b) for a in HJZ_lrs for b in HJZ_other_lrs]
 
 HJZ_lrs_reduced = [0.9, 0.95]
-HJZ_other_lrs_reduced = [0.9, .95, 0.98]
+HJZ_other_lrs_reduced = [0.9, 0.95, 0.98]
 HJZ_lr_pairs_reduced = [(a, b) for a in HJZ_lrs_reduced for b in HJZ_other_lrs_reduced]
 
 HJZ_DEFAULT = [
     {
-        'type': 'HJZ',
-        'params': [{
-            'algorithm': alg,
-            'other_algorithm': 'None',
-            'lr': a,
-            'other_lr': 0,
-            'n_bins': 10,
-            'iterations': 30,
-        } for alg in HJZ_ALGS for a in HJZ_lrs_reduced]
+        "type": "HJZ",
+        "params": [
+            {
+                "algorithm": alg,
+                "other_algorithm": "None",
+                "lr": a,
+                "other_lr": 0,
+                "n_bins": 10,
+                "iterations": 30,
+            }
+            for alg in HJZ_ALGS
+            for a in HJZ_lrs_reduced
+        ],
     },
     {
-        'type': 'HJZ',
-        'params': [{
-            'algorithm': alg,
-            'other_algorithm': alg,
-            'lr': a,
-            'other_lr': b,
-            'n_bins': 10,
-            'iterations': 30,
-        } for alg in ["Hedge", "OptimisticHedge"] for a, b in HJZ_lr_pairs_reduced]
-    }
+        "type": "HJZ",
+        "params": [
+            {
+                "algorithm": alg,
+                "other_algorithm": alg,
+                "lr": a,
+                "other_lr": b,
+                "n_bins": 10,
+                "iterations": 30,
+            }
+            for alg in ["Hedge", "OptimisticHedge"]
+            for a, b in HJZ_lr_pairs_reduced
+        ],
+    },
 ]
 
 # standard calibration algorithms
@@ -82,10 +87,7 @@ CALIB_ALGS_DEFAULT = [
     #     'type': 'Platt',
     #     'params': [{}]
     # },
-    {
-        'type': 'Isotonic',
-        'params': [{}]
-    },
+    {"type": "Isotonic", "params": [{}]},
     # {
     #     'type': 'Temp',
     #     'params': [{'temperature': None, 'optimized': True}] +
@@ -96,4 +98,32 @@ CALIB_ALGS_DEFAULT = [
 
 # collect all mcb algorithsm
 # MCB_DEFAULT = HKRR_DEFAULT + HJZ_DEFAULT + CALIB_ALGS_DEFAULT
-MCB_DEFAULT = HKRR_DEFAULT + CALIB_ALGS_DEFAULT
+CASMCBOOST_DEFAULT = [
+    {
+        "type": "CASMCBoost",
+        "params": [
+            {
+                "mcboost_variant": "jinetal",
+                "encode_categorical_variables": True,
+                "monotone_t": None,
+                "num_rounds": 100,
+                "lightgbm_params": None,
+                "early_stopping": True,
+                "patience": 0,
+                "early_stopping_score_func": None,
+                "early_stopping_minimize_score": None,
+                "early_stopping_timeout": 8 * 60 * 60,
+                "save_training_performance": False,
+                "monitored_metrics_during_training": None,
+                "weight_column_name": None,
+                "categorical_feature_column_names": None,
+                "numerical_feature_column_names": None,
+                "auto_infer_column_types": True,
+                "categorical_threshold": 30,
+            }
+        ],
+    }
+]
+
+# collect all mcb algorithsm
+MCB_DEFAULT = HKRR_DEFAULT + CALIB_ALGS_DEFAULT + CASMCBOOST_DEFAULT  # + HJZ_DEFAULT
